c60bda00c397bc31621a70187712355d1aaff49a
Durval Junior <durvblj@gmail.com>
2019-02-04 12:29:11 -0300

    Produto e assiciacao muitos para muitos

--- src/main/java/com/durval/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/durval/cursomc/repositories/ProdutoRepository.java
@@ -1,12 +1,10 @@
 package com.durval.cursomc.repositories;
 
-
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.durval.cursomc.domain.Categoria;
+import com.durval.cursomc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 }
