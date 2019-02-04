0840c955cbd3c926d63e0e704f636376e4c21ea4
Durval Junior <durvblj@gmail.com>
2019-02-04 16:38:47 -0300

    Endpoint / clientes/ {id} disponivel

---
+++ src/main/java/com/durval/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.durval.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.durval.cursomc.domain.Cliente;
+import com.durval.cursomc.repositories.ClienteRepository;
+import com.durval.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {		
+		   Optional<Cliente> obj = repo.findById(id); 		
+		   return obj.orElseThrow(() -> new ObjectNotFoundException(   
+				   		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())); 	
+	}
+		
+}
