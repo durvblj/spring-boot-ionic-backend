4724977adb0a76a6031cac4b619faa2407b3cfbc
{durvblj} <{durvblj@gmail.com}>
2019-02-06 13:16:55 -0300

    Listando todas categorias com DTO

--- src/main/java/com/durval/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/durval/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.durval.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.durval.cursomc.domain.Categoria;
+import com.durval.cursomc.dto.CategoriaDto;
 import com.durval.cursomc.services.CategoriaService;
 
 @RestController
@@ -43,11 +46,18 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
-		Categoria obj = service.find(id);
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDto>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDto> listDto = list.stream().map(obj -> new CategoriaDto(obj)).collect(Collectors.toList());		
+		return ResponseEntity.ok().body(listDto);
+	}
+
 }
--- src/main/java/com/durval/cursomc/services/CategoriaService.java
+++ src/main/java/com/durval/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.durval.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,5 +43,10 @@ public class CategoriaService {
 
 			throw new DateIntegrityException("Não é possivel excluir uma categoria que possui produtos");
 		}
+	}
+
+	public List<Categoria> findAll() {
+
+		return repo.findAll();
 	}
 }
