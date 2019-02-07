a15305339fe57b7679a1d36bd12d7e228b2e1213
{durvblj} <{durvblj@gmail.com}>
2019-02-07 09:34:11 -0300

    Correcoes update em categoria service e construtor de pagamento

--- src/main/java/com/durval/cursomc/services/CategoriaService.java
+++ src/main/java/com/durval/cursomc/services/CategoriaService.java
@@ -35,8 +35,9 @@ public class CategoriaService {
 	}
 
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());		
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 
 	public void delete(Integer id) {
@@ -67,7 +68,11 @@ public class CategoriaService {
 		
 	}
 	
-	
+	private void updateData(Categoria newObj, Categoria obj) {
+		
+		newObj.setNome(obj.getNome());
+		
+	}
 	
 	
 	
